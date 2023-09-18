eeab9e180540417730ee2804252e48906b8348b5
JulianW051 <julianwessels1@gmail.com>
2023-09-18 13:46:49 +0200

    berekening

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,35 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+    public static double berekenMaxTeLenenBedrag(double maandinkomen, boolean heeftPartner, boolean heeftStudieschuld, int rentevastePeriode, String postcode) {
+        if (maandinkomen <= 0 || (rentevastePeriode != 1 && rentevastePeriode != 5 && rentevastePeriode != 10 && rentevastePeriode != 20 && rentevastePeriode != 30)) {
+            throw new IllegalArgumentException("Ongeldige invoer");
+        }
+
+        if (postcode.equals("9679") || postcode.equals("9681") || postcode.equals("9682")) {
+            return 0.0;
+        }
+
+        double rentePercentage = switch (rentevastePeriode) {
+            case 1 -> 0.02;
+            case 5 -> 0.03;
+            case 10 -> 0.035;
+            case 20 -> 0.045;
+            case 30 -> 0.05;
+            default -> throw new IllegalArgumentException("Ongeldige rentevaste periode");
+        };
+
+        double maxHypotheek = maandinkomen * 12 * rentevastePeriode / (1 - Math.pow(1 + rentePercentage / 12, -rentevastePeriode * 12));
+
+        if (heeftPartner) {
+            maxHypotheek += maxHypotheek * 0.2;
+        }
+        if (heeftStudieschuld) {
+            maxHypotheek *= 0.75;
+        }
+
+        return Math.round(maxHypotheek * 100) / 100.0;
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,45 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testMaxHypotheekMetPartnerEnStudieschuld() {
+        double maxHypotheek = App.berekenMaxTeLenenBedrag(5000, true, true, 20, "1234");
+        assertEquals(360000.0, maxHypotheek, 0.01);
+    }
+
+    @Test
+    public void testMaxHypotheekZonderPartnerEnGeenStudieschuld() {
+        double maxHypotheek = App.berekenMaxTeLenenBedrag(6000, false, false, 10, "1234");
+        assertEquals(720000.0, maxHypotheek, 0.01);
+    }
+
+    @Test
+    public void testMaxHypotheekMetOnjuisteRentevastePeriode() {
+        try {
+            App.berekenMaxTeLenenBedrag(4000, false, true, 7, "1234");
+            fail("Moet een IllegalArgumentException genereren voor ongeldige rentevaste periode");
+        } catch (IllegalArgumentException e) {
+            // Verwachtte uitzondering
+        }
+    }
+
+    @Test
+    public void testMaxHypotheekMetAardbevingsgebied() {
+        double maxHypotheek = App.berekenMaxTeLenenBedrag(5500, false, false, 30, "9679");
+        assertEquals(0.0, maxHypotheek, 0.01);
+    }
+    @Test
+    public void testMaxHypotheekMetVerkeerdeInvoer() {
+        try {
+            App.berekenMaxTeLenenBedrag(-1000, false, false, 5, "1234");
+            fail("Moet een IllegalArgumentException genereren voor ongeldig maandinkomen");
+        } catch (IllegalArgumentException e) {
+            // Verwachtte uitzondering
+
+        }
     }
 }
